5181387d04f4328d426b68c243d843a65c7b9b4f
a7il10 <atilio.sulovari199@gmail.com>
2024-04-22 12:13:21 +0200

    update

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python3
-'''
-Python - Variable Annotations
-'''
+from typing import List, Tuple
 
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    This function takes a list of strings and returns a list of tuples.
+    """
 
-def element_length(lst: List[float]) -> int:
-    '''
-    return values with the appropriate types
-    '''
     return [(i, len(i)) for i in lst]
