08c24e50fda6a245bc8a9c6b41d5a6c3dbd6c6a6
a7il10 <atilio.sulovari199@gmail.com>
2024-04-22 11:43:28 +0200

    ex 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotations
+'''from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''returns their sum as a float'''
+    return sum(input_list)
