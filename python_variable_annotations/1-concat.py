9b0f98bbe687787ed116e2d27fe0d08bea0c0544
a7il10 <atilio.sulovari199@gmail.com>
2024-04-22 11:00:43 +0200

    ex 1, 2

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     '''conacatenates 2 strings'''
     return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def floor(n: float) -> int:
+    '''returns the floor of the float'''
+    floor_value = math.floor(n)
+    return floor_value
