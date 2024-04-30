3a0cbf8a06a07dbc113ebc347e54d7564a582834
a7il10 <atilio.sulovari199@gmail.com>
2024-04-30 11:08:47 +0200

    ex 11 update

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -8,5 +8,5 @@ def schools_by_topic(mongo_collection, topic):
     '''
     function that returns the list of school having a specific topic
     '''
-    value = {"topics": topics}
+    value = {"topics": topic}
     return mongo_collection.find(value)
