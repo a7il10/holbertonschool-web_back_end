039acd1edc1ea9ca059d4813f8ece2aeb63ada59
a7il10 <atilio.sulovari199@gmail.com>
2024-05-04 01:33:16 +0200

    update

--- NoSQL/101-students.py
+++ NoSQL/101-students.py
@@ -3,25 +3,19 @@
 NoSQL
 '''
 
-from operator import itemgetter
-
 
 def top_students(mongo_collection):
     '''
     function that inserts a new document in a collection based on kwargs
     '''
     students = mongo_collection.find()
-    student_scores = []
-
     for student in students:
-
         total_score = 0
-        for course in student['scores']:
-            total_score += course['score']
-        average_score = total_score / len(student['scores'])
-        student_scores.append({'_id': student['_id'], 'name': student['name'], 'averageScore': average_score})
+        count = 0
+        for topic in student['topics']:
+            total_score += topic['score']
+            count += 1
+        student['averageScore'] = total_score / count
 
-    # Sort students by average score in descending order
-    student_scores.sort(key=itemgetter('averageScore'), reverse=True)
-
-    return student_scores
+    sorted_students = sorted(students, key=lambda student: student['averageScore'], reverse=True)
+    return sorted_students
